a45bc86e7f9870c63e8e34216a239a6ae4c177e3
klu <sreevatsalsreevatsal@gmail.com>
2026-03-13 10:53:46 +0530

    Fsad Exam

--- src/main/java/com/klef/fsad/exam/Booking.java
+++ src/main/java/com/klef/fsad/exam/Booking.java
@@ -1,5 +1,55 @@
 package com.klef.fsad.exam;
 
-public class Booking {
+import jakarta.persistence.*;
 
+
+
+@Entity
+public class Booking {
+	@Id
+	private int id;
+	
+	@Override
+	public String toString() {
+		return "Booking [id=" + id + ", name=" + name + ", date=" + date + ", status=" + status + "]";
+	}
+	@Column(nullable=false)
+	private String name;
+	@Column(nullable=false)
+	private String date;
+	@Column(nullable=false)
+	private boolean status;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
 }
--- src/main/java/com/klef/fsad/exam/ClientDemo.java
+++ src/main/java/com/klef/fsad/exam/ClientDemo.java
@@ -1,5 +1,62 @@
 package com.klef.fsad.exam;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
 public class ClientDemo {
 
+	public static void main(String args[]) {
+		addBooking();
+		viewAllBookings();
+	}
+
+	public static void addBooking() {
+
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml"); 
+		
+
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction t = session.beginTransaction();
+
+		Booking b = new Booking();
+		b.setId(1);
+		b.setName("Jaydev");
+		b.setDate("2026-03-13");
+		b.setStatus(true);
+
+		session.save(b);
+
+		t.commit();
+		session.close();
+		sf.close();
+	}
+
+	public static void viewAllBookings() {
+
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		
+
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session session = sf.openSession();
+
+		String hql = "from Booking";   
+		Query<Booking> query = session.createQuery(hql, Booking.class);
+
+		List<Booking> bookings = query.list();
+
+		for(Booking b : bookings) {
+			System.out.println(b);
+		}
+
+		session.close();
+		sf.close();
+	}
 }
